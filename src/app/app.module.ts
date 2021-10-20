6e8f1c86c75cb365b6683448be2afb48986b0025
Lucas Seara Manoel <ls.manoel@gmail.com>
2021-10-20 09:38:01 -0300

    add:add wrapper layer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { P5wrapperComponent } from './p5wrapper/p5wrapper.component';
+import { P5wrapperComponent } from './p5wrapper/wrapper/p5wrapper.component';
 
 @NgModule({
   declarations: [
